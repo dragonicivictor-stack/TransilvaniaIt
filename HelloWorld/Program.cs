22bf690d29eac5bc5ab24c1a820c55227d11daca
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-01 20:27:39 +0300

    Gracefully handle exception

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter yout name: ");
+            Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello,  " + name);
+
+            Console.Write("Please enter yout age: ");
+            string textRead = Console.ReadLine();
+            bool canConvert = int.TryParse(textRead, out int result);
+            if (canConvert)
+            {
+                Console.WriteLine("Hello,  " + name + " , your age is " + result);
+            } 
+            else
+            {
+                Console.WriteLine("The text " + textRead + "doesn't represent a number.");
+            }
+            }
+                
+
+            
+
+           
         }
     }
-}
