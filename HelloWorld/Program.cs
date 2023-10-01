52153b84d152eb29969016027707771dd06379c8
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-01 19:55:36 +0300

    demonstrate read from keyboard

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello,  " + name);
         }
     }
 }
